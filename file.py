7c3957a76ae801ca066afd4f458f252131e4f1f7
sakshiraundal3 <sakshiraundal3@gmail.com>
2026-05-11 13:40:59 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,39 @@
+import random
+import string
+
+def generate_password(length=12):
+    if length < 4:
+        return "Password length should be at least 4"
+
+    # Character sets
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    symbols = string.punctuation
+
+    
+    password = [
+        random.choice(lowercase),
+        random.choice(uppercase),
+        random.choice(digits),
+        random.choice(symbols)
+    ]
+
+    
+    all_characters = lowercase + uppercase + digits + symbols
+    password += random.choices(all_characters, k=length - 4)
+
+    
+    random.shuffle(password)
+
+    return ''.join(password)
+
+print("=== Password Generator ===")
+
+try:
+    length = int(input("Enter password length: "))
+    password = generate_password(length)
+    print("Generated Password:", password)
+
+except ValueError:
+    print("Please enter a valid number.")
